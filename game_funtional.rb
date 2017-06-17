af38e46c81bde100a3b93cc32001662089b3cbe0
Ana Paula Mello <apcomello@gmail.com>
2017-06-17 16:22:40 -0300

    Refactoring to make it more functional (?)

--- game_funtional.rb
+++ game_funtional.rb
@@ -2,30 +2,43 @@
 
 kindergarten = [[1, 1, 0, 0, 0, 0], [1, 0, 1, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
 
-
-def getLiveNeighbours(kindergarten, gem)
-
+def getPossibleCoordinates(gem)
     # Gets the possible x and y values of the neighbours
-    coordinates_set = gem.map {
+
+    gem.map {
     
         |coordinates| [coordinates-1, coordinates, coordinates+1]
     
     }
+
+end
+
+
+def generateNeighbours(kindergarten, gem, coordinates_set)
+
+    isCurrentGem = lambda {|x, y, gem| gem[0] == x and gem[1] == y}
+    isOutsideLimits = lambda {|x, y, kindergarten| x < 0 or y < 0 or x >= kindergarten.length or y >= kindergarten.length}
+
+    coordinates_set[0].map {
         
-    # Combine x and y coordinates to generate the neighbours
-    neighbours = coordinates_set[0].map {
-    
-        |x| coordinates_set[1].map {
-            
-            |y| [x, y] unless (x == gem[0] and y == gem[1]) or (x < 0 or y < 0) or (x >= kindergarten.length or y >= kindergarten.length) # Eliminates the current gem and edge cases
-            
+            |x| coordinates_set[1].map {
+                            
+                |y| [x, y] unless isCurrentGem.call(x, y, gem) or isOutsideLimits.call(x, y, kindergarten)
+                
         }.compact
-    
+        
     }.flatten(1)
+
+end
+
+def getLiveNeighbours(kindergarten, gem)
+
+    isAlive = lambda { |neighbour, kindergarten| kindergarten[neighbour[0]][neighbour[1]] == 1}
     
-    # Selects only the live cells
-    neighbours.select {
-        |live_neighbour| live_neighbour if kindergarten[live_neighbour[0]][live_neighbour[1]] == 1
+    generateNeighbours(kindergarten, gem, getPossibleCoordinates(gem)).select {
+    
+        |neighbour| neighbour if isAlive.call(neighbour, kindergarten)
+        
     }.compact
 
 end
@@ -38,16 +51,12 @@ def canLive(kindergarten, gem)
     return 1
 end
 
-def canEmerge(kindergarten, gem)
-    return 1
-end
-
 def evolveKindergarten(kindergarten)
 
         (0..kindergarten.length-1).map{
             |x| (0..kindergarten.length-1).map{
                 |y|
-                
+                                
                 if kindergarten[x][y] == 1
                     if getLiveNeighbours(kindergarten, [x, y]).length > 3 or getLiveNeighbours(kindergarten, [x, y]).length < 2
                         mustDie(kindergarten, kindergarten[x][y])
@@ -56,7 +65,7 @@ def evolveKindergarten(kindergarten)
                     end
                 else kindergarten[x][y] == 0
                     if getLiveNeighbours(kindergarten, [x, y]).length == 3
-                        canEmerge(kindergarten, kindergarten[x][y])
+                        canLive(kindergarten, kindergarten[x][y])
                     else
                         kindergarten[x][y]
                     end
